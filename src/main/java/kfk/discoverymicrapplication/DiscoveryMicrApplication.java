f09a15456b9ca9aefdc733db14ec3dbca061c368
OleksandrRym <lostmannight@gmail.com>
2025-01-15 21:42:30 +0200

    init

---
+++ src/main/java/kfk/discoverymicrapplication/DiscoveryMicrApplication.java
@@ -0,0 +1,15 @@
+package kfk.discoverymicrapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryMicrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryMicrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kfk/discoverymicrapplication/DiscoveryMicrApplicationTests.java
@@ -0,0 +1,13 @@
+package kfk.discoverymicrapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryMicrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
